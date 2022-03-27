e2b75f14e70490f3cf07fcb44d7fcd255038f46c
thethan <thethan.code@gmail.com>
2022-03-27 23:27:52 +0630

    feat: Add intersection observer to progress bar

--- vendor/progress-bar-counter/animated-counter-prograssbar.js
+++ vendor/progress-bar-counter/animated-counter-prograssbar.js
@@ -4,34 +4,58 @@
  * Author URL: https://github.com/shakibdshy
  * Version: 1.0.0
  */
+// console.log("hello");
+window.addEventListener("load", () => {
 
-;(function($) {
-    'use strict'
+    const skillSection = document.getElementById("skill-section");
+    
+    // configure observer
+    const options = {
+        rootMargin: '0px',
+        threshold: 1.0
+    }
 
-    // Animated Prograssbar
-    $("[progress-bar]").each(function () {
-    $(this)
-        .find(".progress-fill")
-        .animate(
-        {
-            width: $(this).attr("data-percentage"),
-        },
-        2000
-        );
+    // establish observer
+    const observer = new IntersectionObserver((entries) => {
 
-    $(this)
-        .find(".progress-number-mark")
-        .animate(
-        { left: $(this).attr("data-percentage") },
-        {
-            duration: 2000,
-            step: function (now, fx) {
-            var data = Math.round(now);
-            $(this)
-                .find(".percent")
-                .html(data + "%");
-            },
-        }
-        );
-    });
-})(jQuery)
+        entries.forEach(entry => {
+            if (entry.isIntersecting)
+            {
+                (function($) {
+                    'use strict'
+                
+                    $("[progress-bar]").each(function () {
+                        $(this)
+                            .find(".progress-fill")
+                            .animate(
+                            {
+                                width: $(this).attr("data-percentage"),
+                            },
+                            2000
+                            );
+                    
+                        $(this)
+                            .find(".progress-number-mark")
+                            .animate(
+                            { left: $(this).attr("data-percentage") },
+                            {
+                                duration: 2000,
+                                step: function (now, fx) {
+                                var data = Math.round(now);
+                                $(this)
+                                    .find(".percent")
+                                    .html(data + "%");
+                                },
+                            }
+                            );
+                        });
+                
+                })(jQuery);
+            }
+            console.log("Intersected !!!");
+        })
+    }, options);
+    observer.observe(skillSection);
+
+});
+
