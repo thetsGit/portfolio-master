97bdcce50fb3af2a73c146a395c1397e58f1148a
thethan <thethan.code@gmail.com>
2022-04-07 18:37:31 +0630

    Feat: Add real data part 3

--- js/index.js
+++ js/index.js
@@ -1,17 +1,14 @@
-const progressIndicator = document.getElementById("progress-indicator");
+
+// transform state
+window.onload = () => {
+
 const mainContent = document.getElementById("main-content");
 const name = document.getElementById("name");
 const email = document.getElementById("email-me");
 const phone = document.getElementById("phone-me");
-const coffeeShopContent = document.getElementById("coffee-shop-content");
 const projectContents = document.querySelectorAll(".project-content");
-const blogContent = document.getElementById("blog-content");
 const contactAnimates = document.querySelectorAll(".contact-animate");
-const sendBtn = document.getElementById("send-btn");
 const loadingState = document.getElementById("loading-state");
-
-// transform state
-window.onload = () => {
 
   // toggle state
   loadingState.style.display = "none";
@@ -95,40 +92,9 @@ window.onload = () => {
     offset: "75%",
   });
   });
-  let blogWaypoint = new Waypoint({
-  element: blogContent,
-  handler: function () {
-    anime
-      .timeline({ easing: "easeInOutBounce" })
-      .add({
-        targets: "#blog-img",
-        translateY: [200, 0],
-        translateX: [200, 0],
-        duration: 1300, // opacity: [0.5, 1],
-        scale: [3, 1],
-      })
-      .add({
-        targets: "#blog-title",
-        opacity: [0, 1],
-        duration: 500,
-      })
-      .add({
-        targets: "#blog-content",
-        opacity: [0, 1],
-        duration: 500,
-      })
-      .add({
-        targets: "#blog-link",
-        duration: 1000,
-        translateX: [1000, 0],
-        rotate: "3turn",
-      });
-    this.destroy();
-  },
-  offset: "75%",
-  });
+
   contactAnimates.forEach((val, index, list) => {
-  let waypoint = new Waypoint({
+  let waypoint = new waypoint({
     element: val,
     handler: function () {
       anime({
@@ -148,15 +114,11 @@ window.onload = () => {
   });
   });
 
-  setInterval(() => {
-  anime({
-    targets: "#send-btn",
-    translateX: [0, -10],
-    loop: true,
-    direction: "alternate",
-    easing: "easeOutBack",
-    duration: 30,
-    loop: 10,
+  console.log("hello");
+  new VenoBox({
+    selector: ".venobox",
+    spinner: "wander",
+    maxWidth: "100%"
   });
-  }, 5000);
+
 };
